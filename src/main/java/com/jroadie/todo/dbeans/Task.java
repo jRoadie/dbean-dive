7394f45f6b113c64babef62624a96a4c2302b9ef
Sohag <akterhossain.dsi@gmail.com>
2015-07-31 20:41:51 +0600

    updating...

--- src/main/java/com/jroadie/todo/dbeans/Task.java
+++ src/main/java/com/jroadie/todo/dbeans/Task.java
@@ -2,14 +2,47 @@ package com.jroadie.todo.dbeans;
 
 import io.dbean.core.DBean;
 
-import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Task extends DBean {
 
     private String title;
     private String description;
-    private Date dateToComplete;
-    private Date created;
-    private Date updated;
+    private Category category;
+    private Set<Tag> tags = new HashSet<>();
+
+    public String title() {
+        return title;
+    }
+
+    public void title(String title) {
+        this.title = title;
+    }
+
+    public String description() {
+        return description;
+    }
+
+    public void description(String description) {
+        this.description = description;
+    }
+
+    
+    public Category category() {
+        return category;
+    }
+
+    public void category(Category category) {
+        this.category = category;
+    }
+
+    public Set<Tag> tags() {
+        return tags;
+    }
+
+    public void tags(Set<Tag> tags) {
+        this.tags = tags;
+    }
 
 }
